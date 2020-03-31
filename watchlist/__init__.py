f25a0565bd3ec45431c801ce5c2aa61f732f98f0
Leon <leon0116cn@sina.com>
2020-03-31 15:56:24 +0800

    添加了数据库相关操作

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -5,21 +5,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
-name = 'LV'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-
-
 WIN = sys.platform.startswith('win')
 if WIN:
     prefix = 'sqlite:///'
@@ -28,19 +13,24 @@ else:
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(
+    os.path.dirname(app.root_path), 'data.db'
+    )
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = db.session.query(User).first()
+    movies = db.session.query(Movie).all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')
-def init_db(drop):
+def initdb(drop):
     '''Initialize the database.'''
     if drop:
         db.drop_all()
@@ -49,13 +39,44 @@ def init_db(drop):
     click.echo('Initialized the database.')
 
 
+@app.cli.command()
+def forge():
+    '''Generate fake data.'''
+    db.create_all()
+
+    name = 'Leon Lv'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    db.session.add(User(name=name))
+    for movie in movies:
+        db.session.add(Movie(**movie))
+    
+    db.session.commit()
+    click.echo('Done.')
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(50))
 
+    def __repr__(self):
+        return '<User(id={}, name={})>'.format(self.id, self.name)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     year = db.Column(db.String(4))
 
+    def __repr__(self):
+        return '<Movie(id={}, title={}, year={})>'.format(self.id, self.title, self.year)
